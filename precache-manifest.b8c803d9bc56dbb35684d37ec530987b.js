4af84338fd149001fa7ca4946b64dc0d7cb928bb
Charlotte Isambert <charlotte.isambert@gmail.com>
2020-06-05 07:33:19 +0200

    Updates

---
+++ precache-manifest.b8c803d9bc56dbb35684d37ec530987b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f74094aaf68a1be81b1653c4f3f4295",
+    "url": "/portfolio3/index.html"
+  },
+  {
+    "revision": "518a6fefc5a0613baf2a",
+    "url": "/portfolio3/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "cfb069e0ea40bce8d0b8",
+    "url": "/portfolio3/static/js/2.87b508df.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/portfolio3/static/js/2.87b508df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "518a6fefc5a0613baf2a",
+    "url": "/portfolio3/static/js/main.a9fe416f.chunk.js"
+  },
+  {
+    "revision": "aa72f9bba0bf975575f0",
+    "url": "/portfolio3/static/js/runtime-main.bccf3118.js"
+  },
+  {
+    "revision": "2cee2f19f75597051a4b674799ec779b",
+    "url": "/portfolio3/static/media/CV_picture.2cee2f19.jpg"
+  }
+]);
